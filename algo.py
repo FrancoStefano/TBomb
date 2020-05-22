48de1c676a4bcf1d96d613e9ec1fc6bb57b0f086
akromos <baciocco.franco@gmail.com>
2020-05-22 12:35:09 -0500

    terminar esta wea algun dia!"

---
+++ algo.py
@@ -0,0 +1,10 @@
+# !/usr/bin/python3
+# -*- coding:utf-8-*-
+
+
+import base64
+import codecs
+###base64.b64encode(bytes(bomber1, 'utf-8'))
+# base64.b64encode(bytes('your string', 'utf-8'))
+base64.b64decode('bomber.txt')
+codecs.decode('bomber.txt')
